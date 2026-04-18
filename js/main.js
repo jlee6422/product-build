73747226985fd9ea455d15e040f38c34460b5f21
jlee6422 <jlee6422@gmail.com>
2026-04-17 23:26:05 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ctaBtn = document.getElementById('cta-btn');
+
+  ctaBtn?.addEventListener('click', () => {
+    alert('Hello! Ready to build something great?');
+  });
+});
